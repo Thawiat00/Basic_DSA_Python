cc2cddd83e9e6607afe84b72d3b91bc92f68ae0e
Thawiat00 <Thawiat01@gmail.com>
2025-11-18 23:20:50 +0700

    update hash table basic and hash table add
    update hash table basic and hash table add

---
+++ Hash_Table_basic.py
@@ -0,0 +1,12 @@
+# Step 1: สร้าง list ว่าง
+my_list = [None] * 10
+
+# Step 2: สร้าง hash function
+def hash_function(value):
+    sum_of_chars = 0
+    for char in value:
+        sum_of_chars += ord(char)  # Unicode ของตัวอักษร
+    return sum_of_chars % 10       # คืนค่า 0-9
+
+# ทดสอบ
+print("'Bob' มี hash code:", hash_function('Bob'))  # Output: 5
